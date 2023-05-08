2a1efc8d4a72efcde8cba57cca3b7a8f0f588df8
AmanTrivedi1 <amantrivedi598@gmail.com>
2023-05-08 23:11:36 +0530

    ui finished for navigation

---
+++ app/components/Avtar.tsx
@@ -0,0 +1,15 @@
+"use client";
+import Image from "next/image";
+const Avtar = () => {
+  return (
+    <Image
+      className="rounded-full"
+      height="38"
+      width="30"
+      alt="Avtar"
+      src="/images/placeholder.jpg"
+    />
+  );
+};
+
+export default Avtar;
--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -1,6 +1,15 @@
 "use client";
 import { AiOutlineMenu } from "react-icons/ai";
+import Avtar from "../Avtar";
+import { useCallback, useState } from "react";
+import MenuItem from "./MenuItem";
 const UserMenu = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleOpen = useCallback(() => {
+    setIsOpen((value) => !value);
+  }, []);
+
   return (
     <>
       <div className="relative">
@@ -10,18 +19,34 @@ const UserMenu = () => {
             className="hidden md:block text-sm font-semibold py-3
               px-4 rounded-full hover:bg-neutral-100 transition cursor-pointer"
           >
-            Airbnd Your Home{" "}
-            <div
-              className="p-4 md:py-1 md:px-1 border-[1px] border-neutral-100
+            Airbnd Your Home
+          </div>
+          <div
+            className="p-4 md:py-1 md:px-1 border-[1px] border-neutral-100
               rounded-full cursor-pointer  hover:shadow-md transition
               flex flex-row items-center gap-3"
-              onClick={() => {}}
-            >
-              {/* <AiOutlineMenu /> */}
-              <div className="hidden md:block"></div>
+            onClick={toggleOpen}
+          >
+            <AiOutlineMenu />
+            <div className="hidden md:block">
+              <Avtar />
             </div>
           </div>
         </div>
+        {isOpen && (
+          <div
+            className="absolute rounded-xl shadow-md
+              w-[40vw] md:w-3/4 bg-white overflow-hidden right-0 top-12 text-sm
+             "
+          >
+            <div className="flex flex-col cursor-pointer">
+              <>
+                <MenuItem onClick={() => {}} label="Login" />
+                <MenuItem onClick={() => {}} label="Sign up" />
+              </>
+            </div>
+          </div>
+        )}
       </div>
     </>
   );
