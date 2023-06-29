0155e37af26a1a8ef23061eab4f256a730ad2fe5
AmanTrivedi1 <amantrivedi598@gmail.com>
2023-06-29 15:45:52 +0530

    working on the layout

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -3,7 +3,9 @@ import { AiOutlineMenu } from "react-icons/ai";
 import Avtar from "../Avtar";
 import { useCallback, useState } from "react";
 import MenuItem from "./MenuItem";
+import useRegisterModal from "@/app/hooks/useRegisterModal";
 const UserMenu = () => {
+  const registerModal = useRegisterModal();
   const [isOpen, setIsOpen] = useState(false);
 
   const toggleOpen = useCallback(() => {
@@ -19,7 +21,7 @@ const UserMenu = () => {
             className="hidden md:block text-sm font-semibold py-3
               px-4 rounded-full hover:bg-neutral-100 transition cursor-pointer"
           >
-            Airbnd Your Home
+            Airbnb Your Home
           </div>
           <div
             className="p-4 md:py-1 md:px-1 border-[1px] border-neutral-100
@@ -42,7 +44,7 @@ const UserMenu = () => {
             <div className="flex flex-col cursor-pointer">
               <>
                 <MenuItem onClick={() => {}} label="Login" />
-                <MenuItem onClick={() => {}} label="Sign up" />
+                <MenuItem onClick={registerModal.onOpen} label="Sign Up" />
               </>
             </div>
           </div>
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,6 @@
+import ClientOnly from "./components/ClientOnly";
+import Modal from "./components/modals/Modal";
+import RegisterModal from "./components/modals/RegisterModal";
 import Navbar from "./components/navbar/Navbar";
 import "./globals.css";
 import { Nunito } from "next/font/google";
@@ -19,7 +22,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={font.className}>
-        <Navbar />
+        <ClientOnly>
+          <RegisterModal />
+          <Navbar />
+        </ClientOnly>
         {children}
       </body>
     </html>
