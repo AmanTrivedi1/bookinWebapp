2691e32658702a5060c9ae6ffd323c7a1439eb19
AmanTrivedi1 <amantrivedi598@gmail.com>
2023-07-03 15:33:51 +0530

    Removing google and github login

--- app/components/modals/RegisterModal.tsx
+++ app/components/modals/RegisterModal.tsx
@@ -90,7 +90,7 @@ const RegisterModal = () => {
   const footerContent = (
     <div className="flex flex-col gap-4 mt-3">
       <hr />
-      <Button
+      {/* <Button
         outline
         label="Continue with Google"
         icon={FcGoogle}
@@ -101,7 +101,7 @@ const RegisterModal = () => {
         label="Continue with Github"
         icon={AiFillGithub}
         onClick={() => signIn("github")}
-      />
+      /> */}
       <div
         className="
           text-neutral-500 
